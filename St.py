0ee0ca355c2c6a195eb9ba6dcab699436a27b231
Shashank-alpha-cloud <shashankofficial44@gmail.com>
2025-10-16 11:18:58 +0530

    Add files via upload

---
+++ St.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title("Simple Command App")
+
+st.session_state.messages=[]
+
+a=st.text_input("Enter a command:")
+
+if st.button("Run Command"):
+
+	if a.lower() == "hi":
+
+		st.session_state.messages.append("Hello!")
+
+	elif a.lower() == "bye": 
+
+		st.session_state.messages.append("Goodbye!")
+
+	else: 
+
+		st.session_state.messages.append("Unknown command. Try 'hi', 'bye' ")
+
+st.subheader("Output:")
+
+for msg in st.session_state.messages:
+
+	st.text(msg)
